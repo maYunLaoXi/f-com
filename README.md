d78693b611ddfb81bad6443c2adb05f3dc1c0101
liangyh <liangyh@spinsoft.com>
2020-09-27 11:22:09 +0800

    采用rollup构建,更新说明文档

--- README.md
+++ README.md
@@ -1,35 +1,19 @@
 # 开发常用到的工具集合
 
-下载文件到地本地，软链接至项目根目录中
-
-将代码克隆到本地文件夹（以`code`文件夹为例）
-
-*MacOS*
-
-假设code目录为`/Users/mac/Documents/code`，项目目录为`/Users/mac/Documents/program/vue-helloword`
 
 ```
-ln -s /Users/mac/Documents/code/f-com /Users/mac/Documents/program/
+npm install f-com
+```
+or
+```
+yarn add f-com
 ```
 
-如果没有权限侧执行：
-
+## 数组
+### 去重
 ```
-sudo ln -s /Users/mac/Documents/code/f-com /Users/mac/Documents/program/
+import { arrayRemoveRepeat, ... } from 'f-com'
 ```
-
-*Windows*
-
-假设code目录为`D:\script\code` ,项目日录为`D:\program\vue-helloword`
-
-cmd:
-
 ```
-mklink /j D:\script\code\f-com D:\program\vue-helloword
-```
-
-powershell:
-
-```
-cmd /c mklink /j D:\script\code\f-com D:\program\vue-helloword
+arrayRemoveRepeat([{id: 1}], 'id') // 跟椐id值去重
 ```
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,55 @@
-export default {
+import resolve from 'rollup-plugin-node-resolve'
+import { uglify } from 'rollup-plugin-uglify'
+import babel from 'rollup-plugin-babel'
+import merge from 'webpack-merge'
+import commonjs from 'rollup-plugin-commonjs'
+
+const outputDisk = './dist/'
+
+const config = {
   input: './src/index.js',
   output: {
-    file: 'f-com.js',
-    format: 'cjs'
-  }
+    name: 'tks',
+    file: outputDisk + 'f-com.js',
+    format: 'umd'
+  },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**' // 只编译我们的源代码
+    }),
+    commonjs({
+      include: 'node_modules/**'
+    })
+  ]
 }
+
+const packages = [
+  {},
+  {
+    output: {
+      file: outputDisk + 'f-com.min.js',
+      format: 'umd',
+    },
+    plugins: [
+      uglify()
+    ]
+  },
+  {
+    output: {
+      file: outputDisk + 'f-com.es.js',
+      format: 'es'
+    }
+  },
+  {
+    output: {
+      file: outputDisk + 'f-com.cjs.js',
+      format: 'cjs'
+    }
+  }
+]
+const result = []
+for(let item of packages) {
+  result.push(merge(config, item))
+}
+export default result
