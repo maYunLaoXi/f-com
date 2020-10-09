6b3f90df3014b06ff22d462bf1e0f05f4ff51dde
liangyh <liangyh@spinsoft.com>
2020-10-09 14:12:55 +0800

    添加webpack-dev-server

---
+++ src/pack.js
@@ -0,0 +1,4 @@
+import fCom from './index'
+
+console.log('f-com begin', fCom)
+console.log(fCom.isMobile)
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,28 @@
 const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/pack.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'f-com.js',
     library: 'fCom',
     libraryTarget: 'umd'
-  }
+  },
+  devServer: {
+    // 告诉服务器从哪里提供内容
+    contentBase: false,
+    hot: true,
+    port: 8060,
+    open: true,
+  },
+  devtool: 'cheap-module-eval-source-map',
+  plugins: [
+    new htmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+      inject: true,
+    })
+  ]
 }
