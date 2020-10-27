d4a5a472b95164d1e14ad09278870e1e2bea9cb1
liangyh <liangyh@spinsoft.com>
2020-10-27 11:59:04 +0800

    添加一些userAgent

--- src/utils/userAgent.js
+++ src/utils/userAgent.js
@@ -10,9 +10,19 @@ export const isMobile = !!ua.match(
 export const isWeiXin = lowerUa.indexOf('micromessenger') !== -1;
 // ios终端
 export const isIos = !!ua.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+export const isIOS = /iphone|ipad|ipod|ios/.test(lowerUa)
+// android
+export const isAndroid = lowerUa.indexOf('android') > 0
+
 // 小米手机
 export const isXiaoMi = lowerUa.indexOf('mi ') !== -1;
 // 红米手机
 export const isHongMi = lowerUa.indexOf('redmi') !== -1;
 // 小程序 web-view
 export const isMiniProgram = () => window.__wxjs_environment === 'miniprogram';
+
+export const isIE = lowerUa && /msie|trident/.test(lowerUa)
+export const isIE9 = lowerUa.indexOf('msie 9.0') > 0
+export const isEdge = lowerUa.indexOf('edge/') > 0
+export const isChrome = /chrome\/\d+/.test(lowerUa) && !isEdge
+export const isFF = lowerUa.match(/firefox\/(\d+)/)
--- test/utils.test.js
+++ test/utils.test.js
@@ -1,12 +1,15 @@
-import { format, readableTime, mosaicEmail } from'../src/index.js'
+import { format, readableTime, mosaicEmail, isIE, isFF, isChrome } from'../src/index.js'
 const { gitBaseInfo } = require('../src/node/git')
 
+test('userAgent', () => {
+  expect(isChrome || isFF || isIE).toBe(false)
+})
 test('2020-10-01 12:12:12 to 2020年10月1日', () => {
   expect(format('2020-10-01 12:12:12',1)).toBe('2020年10月1日 12:12:12')
 })
 
 test('readableTime 1秒前', () => {
-  expect(readableTime('2020-09-22')).toBe('18天前')
+  expect(readableTime('2020-09-22')).toBe('1个月前')
 })
 test('gitBaseInfo',  () => {
   let info = gitBaseInfo()
