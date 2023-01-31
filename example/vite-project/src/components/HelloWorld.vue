c19602e56d86216fe42824edfa68d61177e71a8c
maYunLaoXi <feigle2014@outlook.com>
2023-01-31 15:26:54 +0800

    fix f-com引入问题

--- example/vite-project/src/components/HelloWorld.vue
+++ example/vite-project/src/components/HelloWorld.vue
@@ -8,22 +8,22 @@ logGitInfo({ name: 'vite-project' })
 defineProps<{ msg: string }>()
 
 const srcs = [
-    "https://img.zcool.cn/community/01ccd862a6ec5c0002c313f73b210c.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
-    "https://img.zcool.cn/community/01f3d062a6ec600002c45e37966330.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
-    "https://img.zcool.cn/community/01b99962a6ec680002c313f7811013.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
-    "https://img.zcool.cn/community/01778862a6ec6d0002c313f7c23267.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
-    "https://img.zcool.cn/community/01a88d62a6ec710002c313f733fbbd.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
-    "https://img.zcool.cn/community/01205b62a6ec770002c45e37060a36.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
-    "https://img.zcool.cn/community/01e08962a6ec7d0002c313f7ea6b54.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
-    "https://img.zcool.cn/community/01afb662a6ec820002c313f7359613.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
-    "https://img.zcool.cn/community/014d9262a6ec890002c45e3714ffac.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
-    "https://img.zcool.cn/community/01811e62a6ec920002c45e37f52b29.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100"
+  "https://img.zcool.cn/community/01ccd862a6ec5c0002c313f73b210c.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
+  "https://img.zcool.cn/community/01f3d062a6ec600002c45e37966330.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
+  "https://img.zcool.cn/community/01b99962a6ec680002c313f7811013.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
+  "https://img.zcool.cn/community/01778862a6ec6d0002c313f7c23267.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
+  "https://img.zcool.cn/community/01a88d62a6ec710002c313f733fbbd.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
+  "https://img.zcool.cn/community/01205b62a6ec770002c45e37060a36.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
+  "https://img.zcool.cn/community/01e08962a6ec7d0002c313f7ea6b54.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
+  "https://img.zcool.cn/community/01afb662a6ec820002c313f7359613.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
+  "https://img.zcool.cn/community/014d9262a6ec890002c45e3714ffac.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
+  "https://img.zcool.cn/community/01811e62a6ec920002c45e37f52b29.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100"
 ]
 const btn = ref('dowloadZip')
 const dowload = function() {
   btn.value = '正在下载'
   console.log(btn.value)
-  dowloadZip({ src: srcs, JsZip: JSZip, fileName: 'test-dl.zip', removeParams: true }).then(res => {
+  dowloadZip({ src: srcs, JsZip: JSZip, fileName: 'test-dl.zip', removeParams: true }).then(() => {
     btn.value = 'dowloadZip'
   })
 }
--- example/vite-project/src/main.ts
+++ example/vite-project/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { logGitInfo } from 'f-com'
+console.log(logGitInfo({ main: 'main.ts' }))
 
 createApp(App).mount('#app')
